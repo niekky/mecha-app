eda103aa0674aeb1850db39672ded6efd635748a
niekky <trungkiencfc@gmail.com>
2023-10-02 16:38:23 -0700

    First prototype

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,113 +6,159 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className='w-full h-screen bg-orange-700'>
+        
+        <div className="flex flex-col w-full h-1/2 bg-[url('/img/intro.png')] items-center justify-center transition ease-in-out duration-500 hover:scale-110">
+          <div className='flex flex-col w-full h-full backdrop-brightness-50 backdrop-blur-sm transition ease-in-out duration-500 hover:backdrop-blur-none items-center justify-center space-y-8'>
+            <div className='flex-col text-7xl font-bold'>M.E.Ch.A.</div>
+            <div className='flex-col text-5xl font-light'>Movimiento Estudiantil Chicano de Aztlán</div>
+          </div>
+          
+        </div>
+
+        <div className='flex flex-col w-full h-1/2 bg-orange-900 items-center justify-center px-96 space-y-5'>
+          <div className='flex-col text-5xl font-bold '>About us</div>
+          <div className='flex-col text-center text-lg'>Movimiento Estudiantil Chicano de Aztlán (Chicano Student Movement of Aztlán) is a student organization founded to raise Mexican American empowerment by using political action and embrace the Mexican culture, history, education, and political/social consciousness for young Chicano. M.E.Ch.A. is the large Chicano organization which is active and spread around the United States. Not only does it raise awareness and political empowerment but also M.E.Ch.A. supports the Mexican American youth in academics through leadership and organizational activities.</div>
+        </div>
+
+      </div>
+
+      <div className="flex flex-col w-full h-48 bg-[url('/img/switch.png')]">
+        <div className='flex flex-col w-full h-full items-center justify-center backdrop-brightness-50 backdrop-blur-sm'>
+          <div className='flex-col text-3xl font-bold'>La Union Hace La Fuerza!</div>
+          <div className='flex-col text-xl'>Unity makes strength</div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='flex w-full h-fit bg-orange-800'>
+
+        <div className='flex-col w-1/2 bg-orange-700 px-48 space-y-12 py-24'>
+          <div className='text-5xl font-bold'>How big we are?</div>
+          <div className='flex-col space-y-6'>
+            <div className='text-2xl'>M.E.Ch.A. is active and widely available around the United States including both the West and the East of the States, with more than 400 chapters in universities, community colleges, and high schools.</div>
+            <div className='text-2xl'>M.E.Ch.A. in different campuses: </div>
+            <div className='text-2xl font-bold'>
+              <div>- Chicanx/Latinx community (UCSB)</div>
+              <div>- ARIZONA MECHA (U of A)</div>
+              <div>- MEChA de UCLA</div>
+              <div>- MeCHA de ASU</div>
+              <div>- MeCHA at Maricopa Community College</div>
+              <div>- MeCHA at Cesar Chavez High School</div>
+            </div>
+          </div>
+        </div>
+        
+        <div className='flex-col w-1/2 py-36 px-32 justify-center items-center space-y-3'>
+          <div className='transition ease-in-out duration-500 hover:scale-110'> 
+            <div className="bg-[url('/img/maps.png')] bg-cover h-[500px]"></div>
+            <div className='flex justify-center items-center font-bold'>MEChA and earlier Chicano student organizations 1967-2012</div>
+          </div>
+          
+        </div>
+      </div>
+      
+      <div className='flex w-full h-screen bg-orange-900'>
+        <div className='flex-col h-screen w-1/2 bg-orange-900 px-48 py-24 space-y-12'>
+          <div className='text-5xl font-bold'>History of M.E.Ch.A</div>
+          <div className='flex-col space-y-6'>
+            <div className='text-xl space-y-6'>
+              <div>Founded in 1969 at a conference in Santa Barbara, CA, along with the adopted draft Plan Espiritual de Aztlán (Spiritual Plan of Aztlán)</div>
+              <div>Founded based on the belief that tying the students into the large network of activists could help fight against oppression and racism and lead to liberation of Chicano people.</div>
+              <div>Improved further institutionalization for the young Mexican American, which motivated Chicanos to attend to school and pursue higher education.</div>
+              <div className='italic'>"M.E.Ch.A. played a part in the development of Chicana and Chicano student activism both in the university and in Chicano communities at large. M.E.Ch.A. has influenced the curriculum for Chicanas and Chicanos and has many times succeeded in improving the social conditions in the barrio" - Carlos Munoz Jr</div>
+            </div>
+          </div>
+        </div>
+          
+        <div className='flex-col w-1/2 py-36 px-32 bg-orange-700 justify-center items-center space-y-3'>
+          <div className='transition ease-in-out duration-500 hover:scale-110'>
+            <div className="bg-[url('/img/el-plan.jpg')] bg-cover h-[500px]"></div>
+            <div className='flex justify-center items-center text-center font-bold'>Chicano activists rally in front of Campbell Hall in a photo that now appears on the wall of North Hall on the UC Santa Barbara campus.</div>
+          </div>
+          
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex flex-col w-full h-48 bg-[url('/img/switch.png')] bg-cover">
+        <div className='flex flex-col w-full h-full items-center justify-center backdrop-brightness-50 backdrop-blur-sm'>
+        <div className='flex-col text-3xl font-bold'>Por la Raza habla el espiritu</div>
+        <div className='flex-col text-xl'>The spirit speaks for the Race</div>
+        </div>
       </div>
+
+      <div className='flex-col flex w-full  bg-orange-800 items-center space-y-16 px-48 py-24'>
+        <div className='text-6xl font-bold'>M.E.Ch.A. Goals</div>
+        <div className='text-xl text-center space-y-6'>
+          <div>Unite Mexican American students and people into a responsive network to respond to prejudice and racism as a large unit</div>
+          <div>Further socialization and politicization for liberation on all campuses</div>
+          <div>Enhance study programs, curriculum development, entrance programs to welcome more Chicanos into college.</div>
+          <div>Educate Mexican Americans about Chicano culture, history, traditions</div>
+          <div>Promote academic achievement and completion of rising young Chicanos</div>
+          <div>Build relationships between high school and college students</div>
+        </div>
+        <div className='flex-col bg-orange-700 justify-center items-center'>
+          <div className="bg-[url('/img/MEChA_Logo.png')] bg-cover w-[480px] h-[390px]"></div>
+        </div>
+          
+      </div>
+        
+      <div className='flex w-full bg-orange-900'>
+
+        <div className='flex-col w-1/2 py-24 px-48 bg-orange-700 justify-center items-center space-y-6'>
+          <div className='transition ease-in-out duration-500 hover:scale-110'>
+            <div className="bg-[url('/img/asu.jpg')] bg-cover h-[360px]"></div>
+            <div className='flex justify-center items-center text-center font-bold'>MEChA at ASU.</div>
+          </div>
+
+          <div className='transition ease-in-out duration-500 hover:scale-110'>
+            <div className="bg-[url('/img/uofu.jpg')] bg-cover h-[360px]"></div>
+            <div className='flex justify-center items-center text-center font-bold'>MEChA at UofU.</div>
+          </div>
+
+          <div className='transition ease-in-out duration-500 hover:scale-110'>
+            <div className="bg-[url('/img/ucla.jpg')] bg-cover h-[360px]"></div>
+            <div className='flex justify-center items-center text-center font-bold'>MEChA at UCLA.</div>
+          </div>
+          
+        </div>
+
+        <div className='flex-col h-screen w-1/2 bg-orange-900 px-48 py-48 space-y-12'>
+          <div className='text-5xl font-bold'>M.E.Ch.A. Activities</div>
+          <div className='flex-col space-y-6'>
+            <div className='text-xl space-y-6'>
+               <div>General areas that M.E.Ch.A. involves:</div>
+               <div className='flex-col italic '>  
+                  <div>- Policy social and governmental agencies to make them more responsive to the poeple of the barrio</div>
+                  <div>- Carry out research on the econmoic and credit policies of merchants in the barrio and expose fraudulent and exorbitant establishments</div>
+                  <div>- Exchange with junior-high and high-school students, help with their projects, teach them organizational techniques, support their actions</div>
+                  <div>- Spread the message of the movement by speaking, radio, television, local newspapers, underground papers, posters, art, theaters</div>
+                  <div>- Expose discrimination in hiring and renting practices</div>
+               </div>
+               <div>Aside, M.E.Ch.A. also holds social meetings actively on many different campuses around the States. By this, Mexican American students are able to make friends and join the community.</div>
+               <div>M.E.Ch.A. on each campus operates differently, but most of them often participate in activities such as: </div>
+               <div className='flex-col font-bold'>  
+                  <div>- Club Fairs</div>
+                  <div>- Community Events</div>
+                  <div>- National Conference</div>
+               </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div className='flex-col flex w-full h-fit bg-orange-800 items-center py-36 px-96 space-y-12'>
+        <div className='text-5xl font-bold'>Want to be a part of M.E.Ch.A.?</div>
+        <div className='flex-col text-center text-2xl space-y-2'>
+          <div>Reach out to M.E.Ch.A. if you have any interest or want to support and improve the organization as well as the Chicano community as a whole</div>
+          <div>Your help is the big contribution to the bright future of Chicanos</div>
+          <div className='italic text-xl'>"Of the community, for the community"</div>
+        </div>
+      </div>
+
     </main>
+    
   )
 }
